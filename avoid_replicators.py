19fccdc750b4905a9546a3b9400304ca3a60335d
Robert Obryk <robryk@google.com>
2025-02-13 16:27:46 +0100

    bff, logging, ...

--- avoid_replicators.py
+++ avoid_replicators.py
@@ -22,31 +22,40 @@ import os
 # how far do we jump back?
 # do we care how often we test?
 
-CHECKPOINT_DIR = "./tmpcheckpoints"
+# TODO: split checkpoints by attempts (we now overwrite overlapping epochs)
+
+SEED = 2
 DELAY = 512
+LANG = "bff"
 
+CHECKPOINT_DIR = "./avoid-{}-seed{}-delay{}".format(LANG, SEED, DELAY)
 
+os.mkdir(CHECKPOINT_DIR)
 
-params = cubff.SimulationParams()
-params.num_programs = 131072
-params.seed = 0
-params.save_to = CHECKPOINT_DIR
-params.save_interval = params.callback_interval;
+with open(os.path.join(CHECKPOINT_DIR, "log.txt"), "w") as logfile:
 
-language = cubff.GetLanguage("bff_noheads")
+    language = cubff.GetLanguage(LANG)
 
-prev_start = 0
-while True:
-    epoch = 0
-    def callback(state):
-        global epoch
-        print("epoch={}".format(state.epoch), end='\r')
-        epoch = state.epoch
-        return state.higher_entropy > 4.0
-    language.RunSimulation(params, None, callback)
-    new_epoch = epoch - DELAY - 1
-    print("epoch={} seed switch, start epoch={}, diff={}".format(epoch, new_epoch, new_epoch - prev_start))
-    prev_start = new_epoch
-    params.seed += 1
-    params.load_from = os.path.join(CHECKPOINT_DIR, "{:010}.dat".format(new_epoch))
+    params = cubff.SimulationParams()
+    params.num_programs = 131072
+    params.seed = SEED * 1000
+    params.save_to = CHECKPOINT_DIR
+    params.save_interval = params.callback_interval;
+    
+    prev_start = 0
+    while True:
+        epoch = 0
+        def callback(state):
+            global epoch
+            print("epoch={}".format(state.epoch), end='\r')
+            epoch = state.epoch
+            return state.higher_entropy > 3.0
+        language.RunSimulation(params, None, callback)
+        new_epoch = epoch - DELAY - 1
+        logline="epoch={} seed switch, start epoch={}, diff={}".format(epoch, new_epoch, new_epoch - prev_start)
+        print(logline)
+        print(logline, file=logfile, flush=True)
+        prev_start = new_epoch
+        params.seed += 1
+        params.load_from = os.path.join(CHECKPOINT_DIR, "{:010}.dat".format(new_epoch))
 
